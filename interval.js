d7fc3d28e4f1d9a96302940c606f49bc8e64d42b
jameswan141 <70609875+jameswan141@users.noreply.github.com>
2021-10-23 02:50:00 -0400

    commit on "main"

--- interval.js
+++ interval.js
@@ -4,15 +4,15 @@ function showTime() {
     let hour = time.getHours();
     let min = time.getMinutes();
     let sec = time.getSeconds();
-    am_pm = "am";
+    am_pm = "AM";
   
     if (hour > 12) {
         hour -= 12;
-        am_pm = " pm";
+        am_pm = " PM";
     }
     if (hour == 0) {
         hr = 12;
-        am_pm = " am";
+        am_pm = " AM";
     }
   
     hour = hour < 10 ? "0" + hour : hour;
